a651fa6f0a6a19929bc0a579d2a67834e728907f
TwiDev <twidev5@gmail.com>
2022-10-24 23:36:15 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::ptr::null;
 use redis::Commands;
 use dotenv::dotenv;
 
-static DRIVER_VERSION: String = String::from("1.0.2");
+const DRIVER_VERSION: String = String::from("1.0.2");
 
 static mut REDIS_HOST_NAME: String = String::from("127.0.0.1");
 static mut REDIS_PASSWORD: String = String::from("foo");
@@ -16,7 +16,7 @@ fn main() {
 
     unsafe {
         REDIS_HOST_NAME = env::var("REDIS_HOST_NAME").expect("missing environment variable REDIS_HOSTNAME");
-        REDIS_PASSWORD = env::var("REDIS_PASSWOR").expect("missing environment variable REDIS_PASSWORD");
+        REDIS_PASSWORD = env::var("REDIS_PASSWORD").expect("missing environment variable REDIS_PASSWORD");
 
         //Check if redis server needs a secure link for connection
         IS_TLS = match env::var("IS_TLS") {
@@ -28,9 +28,17 @@ fn main() {
 
 pub mod redis_driver {
     use std::env;
+    use crate::{IS_TLS, REDIS_HOST_NAME, REDIS_PASSWORD};
 
     fn connect() -> redis::Connection {
+        unsafe {
+            let redis_conn_url = format!("{}://:{}@{}", IS_TLS, REDIS_PASSWORD, REDIS_HOST_NAME);
 
+            return redis::Client::open(redis_conn_url)
+                .expect("Invalid connection URL")
+                .get_connection()
+                .expect("Failed to connect to Redis")
+        }
     }
 
 }
